69744eb89d40f3e03767178ec587e55c964e493c
abd-cpu <abdigaliyev.arkhat@mail.ru>
2020-11-12 19:40:06 +0600

    commit decript

--- src/Components/Dialogs/Companion/Companion.jsx
+++ src/Components/Dialogs/Companion/Companion.jsx
@@ -1,16 +1,17 @@
 import React from 'react';
 import s from './Companion.module.css';
+import {NavLink} from 'react-router-dom';
 
 const Companion = (props) => {
     return (
-        <div className={s.container}>
+        <NavLink to={props.pathName} className={s.container}>
             <img src={props.avatar} />
             <div>
                 <div className={s.name}>{props.userName}</div>
                 <div className={s.status}>{props.status}</div>
             </div>
             
-        </div>
+        </NavLink>
     )
 }
 
--- src/Components/Dialogs/Dialogs.jsx
+++ src/Components/Dialogs/Dialogs.jsx
@@ -6,14 +6,14 @@ const Dialogs = (props) => {
   return (
     <div className={s.dialogs}>
       <div className={s.dialogList}>
-        <Companion status="online" userName="Bob Marlie" avatar="https://iqonic.design/themes/socialv/vue/dist/img/user-01.22e5a823.jpg?__WB_REVISION__=22e5a82300637450aa1b4f2de5871337" />
-        <Companion status="online" userName="Jim Trevor" avatar="https://iqonic.design/themes/socialv/vue/dist/img/user-03.a25e92f0.jpg?__WB_REVISION__=a25e92f09c9354a61dbcc1150101a9f5" />
-        <Companion status="online" userName="Lin Hue" avatar="https://iqonic.design/themes/socialv/vue/dist/img/user-05.1c0efd4b.jpg?__WB_REVISION__=1c0efd4bac21364863911f337fcd51cf" />
-        <Companion status="offline" userName="Xiao Han" avatar="https://iqonic.design/themes/socialv/vue/dist/img/user-04.ac29a257.jpg?__WB_REVISION__=ac29a2570bc401ae27a4ce915817eaf5" />
-        <Companion status="offline" userName="Chelsea Lee" avatar="https://iqonic.design/themes/socialv/vue/dist/img/user10.db6dc465.jpg?__WB_REVISION__=db6dc4655055421dfdbae689fa57e821" />
+        <Companion pathName="/dialogs/1" status="online" userName="Bob Marlie" avatar="https://iqonic.design/themes/socialv/vue/dist/img/user-01.22e5a823.jpg?__WB_REVISION__=22e5a82300637450aa1b4f2de5871337" />
+        <Companion pathName="/dialogs/2" status="online" userName="Jim Trevor" avatar="https://iqonic.design/themes/socialv/vue/dist/img/user-03.a25e92f0.jpg?__WB_REVISION__=a25e92f09c9354a61dbcc1150101a9f5" />
+        <Companion pathName="/dialogs/3" status="online" userName="Lin Hue" avatar="https://iqonic.design/themes/socialv/vue/dist/img/user-05.1c0efd4b.jpg?__WB_REVISION__=1c0efd4bac21364863911f337fcd51cf" />
+        <Companion pathName="/dialogs/4" status="offline" userName="Xiao Han" avatar="https://iqonic.design/themes/socialv/vue/dist/img/user-04.ac29a257.jpg?__WB_REVISION__=ac29a2570bc401ae27a4ce915817eaf5" />
+        <Companion pathName="/dialogs/5" status="offline" userName="Chelsea Lee" avatar="https://iqonic.design/themes/socialv/vue/dist/img/user10.db6dc465.jpg?__WB_REVISION__=db6dc4655055421dfdbae689fa57e821" />
       </div>
       <div className={s.discussion}>
-        <Companion status="online" userName="Bob Marlie" avatar="https://iqonic.design/themes/socialv/vue/dist/img/user-01.22e5a823.jpg?__WB_REVISION__=22e5a82300637450aa1b4f2de5871337" />
+        <Companion pathName="/dialogs/1" status="online" userName="Bob Marlie" avatar="https://iqonic.design/themes/socialv/vue/dist/img/user-01.22e5a823.jpg?__WB_REVISION__=22e5a82300637450aa1b4f2de5871337" />
         <div className={s.display}>
         </div>
         <div className={s.inputs}>
